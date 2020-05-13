65381a21d0b5d1322feae1e16c191120e727bce6
JMoicano <jmariossilva@gmail.com>
2020-05-13 16:58:48 -0300

    Credit card priming screen layout

---
+++ app/src/main/java/com/jmoicano/desafiopicpay/app/creditcard/CreditCardPrimingActivity.kt
@@ -0,0 +1,13 @@
+package com.jmoicano.desafiopicpay.app.creditcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jmoicano.desafiopicpay.R
+
+class CreditCardPrimingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit_card_priming)
+    }
+}
